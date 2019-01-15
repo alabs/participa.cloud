699fbb91f01e015d0d157508509a9297fa06010f
Mijail Rondon <mijail@riseup.net>
2019-01-15 15:06:19 -0400

    Sidekiq config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,6 +9,7 @@ set :deploy_to, "/home/ruby-data/app"
 set :rbenv_type, :user
 set :delayed_job_workers, 1
 set :rbenv_ruby, "2.5.3"
+set :sidekiq_config, "config/sidekiq.yml"
 
 append :linked_files, "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads"
@@ -17,3 +18,6 @@ append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/syst
 set :puma_bind, "tcp://0.0.0.0:3000"
 set :puma_user, fetch(:user)
 append :rbenv_map_bins, 'puma', 'pumactl'
+
+SSHKit.config.command_map[:sidekiq] = "bundle exec sidekiq"
+SSHKit.config.command_map[:sidekiqctl] = "bundle exec sidekiqctl"
